35c37092cb310c7c2a68a595d58cd5c6c4fff5d0
Flambel SANOU <98210672+FlambelSANOU@users.noreply.github.com>
2025-11-29 22:06:56 +0100

    Add loading state and anonymous auth to services front
    Introduced a loading state with spinner and logo to the services front component. Implemented user verification and automatic anonymous authentication if no user is found. Updated HTML templates to conditionally display content based on loading state. Also fixed translation key usage in services.component.html and added the Yaba-In logo asset.

--- src/app/feature-module/services/service-front/services-front.component.ts
+++ src/app/feature-module/services/service-front/services-front.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Candidate } from './candidate.model';
 import { CandidateService } from './candidate.service';
 import { finalize } from 'rxjs/operators';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { UserService } from 'src/app/services/user/user.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { LanguageService } from 'src/app/services/language/language.service';
 
 @Component({
   selector: 'app-services-front',
@@ -10,16 +16,79 @@ import { finalize } from 'rxjs/operators';
 })
 export class ServicesFrontComponent implements OnInit {
   candidates: Candidate[] = [];
-  loading = false;
   votedId: string | null = null;
   processingVote = false;
   toastMessage = '';
+  url: string = '';
+  idParam: string | null = null;
+  isAdminRoute: boolean = false;
+  loading: boolean = true;
 
 
-  constructor(private candidateService: CandidateService) {}
+  constructor(
+    private location: Location,
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private authService: AuthService,
+    private language: LanguageService,
+  ) {}
 
   ngOnInit(): void {
+    this.verifyUser();
   }
 
 
+
+  getId() {
+    this.url = this.location.path();
+    this.isAdminRoute = this.url.includes('admin-services');
+
+    const idParam = this.route.snapshot.paramMap.get('id');
+    console.log('idParam', idParam);
+
+    if (idParam && idParam !== 'null' && idParam !== 'undefined') {
+      this.idParam = idParam;
+    }
+  }
+
+  verifyUser() {
+    const user = this.userService.getCurrentUserData()
+    .then((user: any) => {
+      console.log('user', user);
+      if (user) {
+        this.loading = false;
+      } else this.authAsAnonymous();
+    })
+    .catch((e) => {
+      this.loading = false;
+      const err = e.error.message.message;
+      console.error('error to login: ', e.error.message.message)
+    });
+  }
+
+
+  authAsAnonymous() {
+    return this.authService.login({
+      type: 'email',
+      email: 'dkuser123@email.com',
+      password: '1234567890',
+    })
+    .then((user: any) => {
+      console.log('user', user);
+      if (user) {
+          this.userService.setCurrentUser(user);
+          this.language.setLocalUserLanguage(user.language);
+          this.language.useLanguage(user.language);
+          this.loading = false;
+        
+      } else {
+        this.loading = false;
+      }
+    })
+    .catch((e) => {
+      this.loading = false;
+      const err = e.error.message.message;
+      console.error('error to login: ', e.error.message.message)
+    });
+  }
 }
