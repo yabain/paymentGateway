54eaebb0c778e8f3f30bac7c06d3221d30e430b4
Flambel SANOU <98210672+FlambelSANOU@users.noreply.github.com>
2026-01-29 03:29:22 +0100

    Internationalize API documentation page
    Refactored the API documentation component and template to use translation keys for all static text, enabling support for English and French. Added language selection UI, integrated language and translation services, and updated i18n files with all API doc content in both languages.

--- src/app/front/api-doc/api-doc.component.ts
+++ src/app/front/api-doc/api-doc.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/services/language/language.service';
+import { ToastService } from 'src/app/services/toast/toast.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -10,11 +13,43 @@ import { environment } from 'src/environments/environment';
 export class ApiDocComponent {
   selectedSection = 'intro'
   frontUrl: string = environment.frontUrl;
+  // en = [{ name: 'lang.en', value: 'en', flagImage: 'assets/icons/uk-flag.svg' }];
+  // fr = [{ name: 'lang.fr', value: 'fr', flagImage: 'assets/icons/fr-flag.svg' }];
 
-  constructor(private router: Router) {}
+  en: any = '';
+  fr: any = '';
+  selectedLanguage: any = '';
+  changingLang: boolean = false;
+
+  constructor(
+    private router: Router,
+    private language: LanguageService,
+    private translate: TranslateService,
+    private toastService: ToastService,
+  ) {
+    this.getLanguage();
+    this.en = this.language.en;
+    this.fr = this.language.fr;
+  }
   
   selectSection(section: string){
     this.selectedSection = section;
+  }
+
+  async getLanguage() {
+    this.selectedLanguage = await this.language.getDefaultLanguage();
+    this.selectedLanguage = this.selectedLanguage === 'en' ? this.en : this.fr;
+  }
+
+  useLanguage(lang) {
+    this.changingLang = true;
+
+        this.language.useLanguage(lang);
+        // this.translate.get('lang.langChanged').subscribe((res: string) => {
+        //   this.toastService.presentToast('success', 'Done!', res, 2000);
+        // });
+        this.getLanguage();
+        this.changingLang = false;
   }
 
   navigateTo(url){
